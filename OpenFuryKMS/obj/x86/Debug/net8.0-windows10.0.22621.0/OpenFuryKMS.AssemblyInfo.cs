91ed906ad3287ab56a269ba97dc76ea71e87d46f
Kevin Greenwood <kevinrecalde324@outlook.com>
2024-10-07 15:09:00 -0500

    Initial commit

--- OpenFuryKMS/obj/x86/Debug/net8.0-windows10.0.22621.0/OpenFuryKMS.AssemblyInfo.cs
+++ OpenFuryKMS/obj/x86/Debug/net8.0-windows10.0.22621.0/OpenFuryKMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenFuryKMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d13fd51533ae6203657498a103d865b010e69b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenFuryKMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenFuryKMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
